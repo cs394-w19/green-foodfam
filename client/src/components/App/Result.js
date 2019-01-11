117878a5dcf3d9577af693f827972c28af845ee7
Ryan McHenry <ryanmchenry2@gmail.com>
2019-01-10 19:00:05 -0600

    working

--- client/src/components/App/Result.js
+++ client/src/components/App/Result.js
@@ -1,18 +1,48 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Link } from 'react-router-dom'
+import Loading from './Loading'
 
 class Result extends Component {
-  state = {
-    data: null
+  constructor(props){
+    super(props);
+    this.state = {
+      data:null,
+      APIsuccess:false
+    }
+  }
+
+  componentWillMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => {
+        console.log(res.express.selection)
+        this.setState({ data: res.express.selection.name, APIsuccess:true })})
+      .catch(err => console.log(err));
+  }
+
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch("/restaurant/select");
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
   };
 
   render() {
-    return (
-      <div className="App">
-        <div className='appRecommend'>We Recommend:</div>
-      </div>
-    );
+    if(this.state.APIsuccess){
+      return (
+        <div className="App">
+          <div className='appRecommend'>We Recommend:</div>
+          <div>{this.state.data}!</div>
+        </div>
+      );
+    }
+    else{
+      return(<Loading/>)
+    }
   }
 }
 
