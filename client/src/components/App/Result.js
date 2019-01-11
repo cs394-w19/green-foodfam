2fec33af7566b9428dfd658d87df77261f4535b4
Ryan McHenry <ryanmchenry2@gmail.com>
2019-01-10 20:28:39 -0600

    results page UI

--- client/src/components/App/Result.js
+++ client/src/components/App/Result.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Loading from './Loading'
+import { Link } from 'react-router-dom'
 
 class Result extends Component {
   constructor(props){
@@ -16,7 +17,20 @@ class Result extends Component {
     this.callBackendAPI()
       .then(res => {
         console.log(res.express.selection)
-        this.setState({ data: res.express.selection.name, APIsuccess:true })})
+        this.setState({
+          data: res.express.selection,
+          APIsuccess:true,
+          name:res.express.selection.name,
+          image_url:res.express.selection.image_url,
+          display_phone:res.express.selection.display_phone,
+          phone:'tel:' + res.express.selection.phone,
+          rating:res.express.selection.rating,
+          url:res.express.selection.url,
+          address1:res.express.selection.location.address1,
+          city:res.express.selection.location.city,
+          state:res.express.selection.location.state,
+          zip_code:res.express.selection.location.zip_code
+        })})
       .catch(err => console.log(err));
   }
 
@@ -32,11 +46,39 @@ class Result extends Component {
   };
 
   render() {
+    var mapText = this.state.address1 + ' ' + this.state.city + ' ' + this.state.state + ' ' + this.state.zip_code
+    var mapWithSpaces = mapText.replace(/ /g, "+");
+    var mapURL = 'https://www.google.com/maps/place/' + mapWithSpaces
+
+    const stars = Array.apply(null, { length: this.state.rating }).map((e, i) => (
+                    <img key={i} src='/star.png' className='star' alt='star'/>
+                  ));
+
     if(this.state.APIsuccess){
       return (
         <div className="App">
           <div className='appRecommend'>We Recommend:</div>
-          <div>{this.state.data}!</div>
+          <img src={this.state.image_url} className='restaurantPic' alt='yelp-pic'/>
+          <div className='restName'>{this.state.name}</div>
+          <div>{this.state.address1}</div>
+          <div>{this.state.city}, {this.state.state} {this.state.zip_code}</div>
+          <div>{stars}</div>
+          <div className='buttonContainer'>
+            <Link to='/'>
+              <button className="containedButton" style={{backgroundColor:'rgba(234, 72, 72, .8)'}}>
+                <img className='containedButtonImg' src='/back.png' alt=''/>
+              </button>
+            </Link>
+            <a href={mapURL} target='_blank' rel="noopener noreferrer"><button className='containedButton' style={{backgroundColor:'rgba(66, 134, 244, .8)'}}>
+              <img className='containedButtonImg' src='/directions.png' alt=''/>
+            </button></a>
+            <a href={this.state.phone}><button className="containedButton" style={{backgroundColor:'rgba(64, 229, 119, .8)'}}>
+              <img className='containedButtonImg' src='/phone.png' alt=''/>
+            </button></a>
+          <a href={this.state.url} target='_blank' rel="noopener noreferrer"><button className="containedButton" style={{backgroundColor:'#fc8888'}}>
+              <img className='containedButtonImg' src='/web.png' alt=''/>
+            </button></a>
+          </div>
         </div>
       );
     }
