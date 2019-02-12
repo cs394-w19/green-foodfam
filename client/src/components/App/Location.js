c46f76111f65dc53bb21a91042d5735990fd6d5d
Vanessa Chu <vanessachu2020@u.northwestern.edu>
2019-02-11 23:26:23 -0600

    geosuggest done

--- client/src/components/App/Location.js
+++ client/src/components/App/Location.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
 import Geosuggest from 'react-geosuggest';
+import "./Geosuggest.css"
 
 class App extends Component {
   state = {
     location: "Evanston, IL"
   };
 
-  updateLocation = e => {
-    this.setState({
-      location: e.target.value
-    });
-  };
+  updateLocation(locname){
+    this.state.location = locname.description
+  }
 
   onComplete(){
     this.props.updateData('location',this.state.location)
@@ -21,10 +20,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="appQuestion">Enter your group's location:</div>
+        <div className="locationInput">
         < Geosuggest
             className="locationInput"
+            placeholder={this.state.location}
             onSuggestSelect={label => this.updateLocation(label)}
+            country='us'
             />
+          </div>
             {/*
         // <input
         //   className="locationInput"
